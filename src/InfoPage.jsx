b10a4d2be17edb176ffcc46b1a922c47916de749
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-18 18:17:27 -0300

    Refactor code structure for improved readability and maintainability

--- src/InfoPage.jsx
+++ src/InfoPage.jsx
@@ -1,15 +1,31 @@
 import GoBack from "./components/GoBack";
 import Header from "./components/Header";
 
-export default function InfoPage(){
-    return(
+export default function InfoPage() {
+    return (
         <>
             <Header></Header>
-            <main className="min-h-dvh flex flex-col items-center py-8 gap-8">
-                <GoBack></GoBack>
+            <GoBack className="pt-10 pb-1 px-14"></GoBack>
+            <main className="min-h-dvh flex flex-col items-center gap-14">
                 <iframe className="rounded-2xl" width="749" height="430" src="https://www.youtube.com/embed/5nL-Eq1lpDU" title="I Fixed YouTube !" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <h1 className="text-5xl font-black">Receipt Name - Main Title</h1>
+                <div className="flex-grid">
+                    <div className="relative w-100 h-auto">
+                        <img className="absolute w-100" src="src/assets/ingredients-note.png"></img>
+                        <div className="absolute left-16 top-24">
+                            <h2>Ingredients</h2>
+                            <p> - Fake</p>
+                            <p> - Fake</p>
+                            <p> - Fake</p>
+                            <p> - Fake</p>
+                            <p> - Fake</p>
+                        </div>
+                    </div>
+                    <img src="src/assets/placeholder-food.jpg"></img>
+                    <div></div>
+                </div>
             </main>
-            <footer>Placeholder footer too</footer>
+            <footer className="flex w-full justify-center py-12">Placeholder footer Bruh</footer>
         </>
     )
 }
--- src/components/GoBack.jsx
+++ src/components/GoBack.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-export default function GoBack(){
+export default function GoBack(props){
     return(
         <>
-            <div className="flex justify-start w-full px-12">
+            <div className={`${props.className} flex justify-start w-full`}>
                 <Link to="/" className="bg-orange-500 p-3 rounded-full
                 font-bold text-white shadow cursor-pointer">← Return</Link>
             </div>
