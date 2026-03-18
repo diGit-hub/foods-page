1375e12f07c0962fa6ac4eaac3557d75237907ff
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-18 01:40:43 -0300

    Add routing and create Home and InfoPage components

---
+++ src/InfoPage.jsx
@@ -0,0 +1,11 @@
+export default function InfoPage(){
+    return(
+        <>
+            <header>Placeholder header while it's not ready</header>
+            <main>
+                
+            </main>
+            <footer>Placeholder footer too</footer>
+        </>
+    )
+}
