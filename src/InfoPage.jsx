37f521d5e1cb75890877e2663dc443640e7e048c
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-18 17:30:08 -0300

    Add GoBack component to navigate back to Home

--- src/InfoPage.jsx
+++ src/InfoPage.jsx
@@ -1,11 +1,13 @@
+import GoBack from "./components/GoBack";
 import Header from "./components/Header";
 
 export default function InfoPage(){
     return(
         <>
             <Header></Header>
-            <main className="min-h-dvh flex flex-col items-center py-12">
-                <iframe width="749" height="430" src="https://www.youtube.com/embed/5nL-Eq1lpDU" title="I Fixed YouTube !" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <main className="min-h-dvh flex flex-col items-center py-8 gap-8">
+                <GoBack></GoBack>
+                <iframe className="rounded-2xl" width="749" height="430" src="https://www.youtube.com/embed/5nL-Eq1lpDU" title="I Fixed YouTube !" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </main>
             <footer>Placeholder footer too</footer>
         </>
--- src/components/GoBack.jsx
+++ src/components/GoBack.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
-export default function GoBack(props){
-return(
-<>
-<div className={`${props.className} flex justify-start w-full`}>
-<Link to="/" className="bg-orange-500 p-3 rounded-full
-font-bold text-white shadow cursor-pointer">← Return</Link>
-</div>
-</>
-)
+export default function GoBack(){
+    return(
+        <>
+            <div className="flex justify-start w-full px-12">
+                <Link to="/" className="bg-orange-500 p-3 rounded-full
+                font-bold text-white shadow cursor-pointer">← Return</Link>
+            </div>
+        </>
+    )
 }
