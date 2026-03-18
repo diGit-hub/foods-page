ba222765a7990451f617e4f219f910b42f9acadd
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-18 16:49:53 -0300

    Resolve conflicts on merge

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,11 @@
+import Carousel from "./components/Carousel";
+import Header from "./components/Header";
+
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <Header></Header>
+            <Carousel></Carousel>
         </>
     );
 }
