53f371a52574e8ee8966fc27b390503083c43979
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-18 17:05:54 -0300

    Added Header component to InfoPage screen

--- src/InfoPage.jsx
+++ src/InfoPage.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 export default function InfoPage(){
     return(
         <>
-            <header>Placeholder header while it's not ready</header>
-            <main>
+            <Header></Header>
+            <main className="min-h-dvh flex flex-col items-center py-12">
                 <iframe width="749" height="430" src="https://www.youtube.com/embed/5nL-Eq1lpDU" title="I Fixed YouTube !" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </main>
             <footer>Placeholder footer too</footer>
