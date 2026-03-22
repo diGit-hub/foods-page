501a632842d4a45fecec103d2cc77acbae0dad15
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-22 02:31:37 -0300

    Replace Link component with button for GoBack functionality to enhance navigation experience

--- src/components/GoBack.jsx
+++ src/components/GoBack.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
-
-export default function GoBack(props){
-    return(
-        <>
-            <div className={`${props.className} flex justify-start w-full`}>
-                <Link to="/" className="bg-orange-500 p-3 rounded-full
-                font-bold text-white shadow cursor-pointer">← Return</Link>
-            </div>
-        </>
-    )
+export default function GoBack(props) {
+    return (
+        <div className={`${props.className} flex justify-start w-full`}>
+            <button
+                onClick={() => window.history.back()}
+                className="bg-orange-500 p-3 rounded-full font-bold text-white shadow cursor-pointer"
+            >
+                ← Return
+            </button>
+        </div>
+    );
 }
